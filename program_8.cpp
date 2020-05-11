91085f2c8e9b38f7950a6a2cda9181939e3ebfdc
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-11 16:23:13 +0530

    program to get input from the user using cin

---
+++ program_8.cpp
@@ -0,0 +1,17 @@
+// getting input from the user
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int age;
+
+	cout << "Please enter your age: " << endl;
+	cin >> age;
+
+	cout << "Your age is " << age << endl;
+
+	return 0;
+}
