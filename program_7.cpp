e0abded724051f045dcb2041efb3da801f61d33c
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-11 16:19:36 +0530

    program to use math library functions over numbers

---
+++ program_7.cpp
@@ -0,0 +1,19 @@
+// use various math library operations on the numbers
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	cout << pow(5, 3) << endl;
+	cout << sqrt(5) << endl;
+	cout << round(5.443) << endl;
+	cout << ceil(5.1) << endl;
+	cout << floor(5.1) << endl;
+	cout << fmax(5, 10) << endl;
+	cout << fmin(5, 10) << endl;
+
+	return 0;
+}
