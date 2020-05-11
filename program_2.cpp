29b163abd504ca150d200b5e890c10cf129c1fbf
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-11 15:36:09 +0530

    program to print out a shape using cout on the console

---
+++ program_2.cpp
@@ -0,0 +1,15 @@
+// print out a shape using cout on the console
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "   /|" << endl;
+	cout << "  / |" << endl;
+	cout << " /  |" << endl;
+	cout << "/___|" << endl;
+
+	return 0;
+}
