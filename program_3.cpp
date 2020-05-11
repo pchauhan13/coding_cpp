1bb227b9946993ff46cf1ddb667eb16b2d8608e7
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-11 16:02:40 +0530

    program to use variables as placeholder

---
+++ program_3.cpp
@@ -0,0 +1,18 @@
+// use variables as placeholder
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string name = "John";
+	int age = 35;
+
+	cout << "There once was a man named " << name << endl;
+	cout << "He was " << age << " years old" << endl;
+	cout << "He liked the name " << name << endl;
+	cout << "But did not like being " << age << endl;
+
+	return 0;
+}
