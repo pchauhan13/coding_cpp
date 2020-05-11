eff9cb3da2f3ca82a90a0abdeb0b7263376d9e18
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-11 16:02:59 +0530

    program to use various type of variables in c++

---
+++ program_4.cpp
@@ -0,0 +1,22 @@
+// using different variable types in c++
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char grade = 'A';
+	string phrase = "Girraffe Academy";
+	int age = 50;
+	double gpa = 2.3;
+	bool isMale = false;
+
+	cout << grade << endl;
+	cout << phrase << endl;
+	cout << age << endl;
+	cout << gpa << endl;
+	cout << isMale << endl;
+
+	return 0;
+}
