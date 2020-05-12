14c3a1c8bf827e7bc2c839aedb3642fc02ccf70e
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-12 11:00:55 +0530

    program to use for loops

---
+++ program_19.cpp
@@ -0,0 +1,17 @@
+// use do..while loop
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	int index = 0;
+
+	do
+	{
+		cout << index++ << endl;
+	} while (index <= 5);
+
+	return 0;
+}
---
+++ program_20.cpp
@@ -0,0 +1,15 @@
+// using for loops
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		cout << i << endl;
+	}
+
+	return 0;
+}
