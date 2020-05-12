cc21aa128ae6f8ab486d49f932c03e8467ae0151
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-12 12:33:10 +0530

    program to illustrate basic inheritance among classes

---
+++ program_30.cpp
@@ -0,0 +1,43 @@
+// demonstrate inheritance among classes in c++
+
+#include <iostream>
+
+using namespace std;
+
+class Chef
+{
+	public:
+		void makeChicken ()
+		{
+			cout << "The chef makes chicken" << endl;
+		}
+		void makeSalad ()
+		{
+			cout << "The chef makes salad" << endl;
+		}
+		void makeSpecialDish ()
+		{
+			cout << "The chef makes bbq ribs" << endl;
+		}
+};
+
+class ItalianChef : public Chef
+{
+	public:
+		void makePasta ()
+		{
+			;cout << "The chef makes pasta" << endl;
+		}
+};
+
+int main ()
+{
+	Chef chef;
+	chef.makeChicken();
+
+	ItalianChef italianChef;
+	italianChef.makeChicken();
+	italianChef.makePasta();
+
+	return 0;
+}
