1724485703ae3bad0d989640d464e10c7c655546
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-12 11:29:30 +0530

    program to create and instantiate a class in c++

---
+++ program_26.cpp
@@ -0,0 +1,26 @@
+// create and instantiate classes in c++
+
+#include <iostream>
+
+using namespace std;
+
+class Book
+{
+	public:
+		string title;
+		string author;
+		int pages;
+};
+
+int main ()
+{
+	Book book1;
+
+	book1.title = "Harry Potter";
+	book1.author = "JK Rowling";
+	book1.pages = 500;
+
+	cout << book1.title << ":" << book1.author << ":" << book1.pages << endl;
+
+	return 0;
+}
