db50d0616ef8e9cbaad49a3b1e294fd1dbdc4cb3
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-12 12:07:20 +0530

    program to add a method to the class and call it over the object of the class

---
+++ program_28.cpp
@@ -0,0 +1,47 @@
+// adding methods to the classes and using them
+
+#include <iostream>
+
+using namespace std;
+
+class Student
+{
+	public:
+		string name;
+		string major;
+		double gpa;
+
+		Student ()
+		{
+			name = "no name";
+			major = "no major";
+			gpa = 0.0;
+		}
+		
+		Student (string aName, string aMajor, double aGpa)
+		{
+			name = aName;
+			major = aMajor;
+			gpa = aGpa;
+		}
+
+		bool hasHonors ()
+		{
+			if (gpa >= 3.5)
+			{
+				return true;
+			}
+			return false;
+		}
+};
+
+int main ()
+{
+	Student student1("Jim", "Business", 2.4);
+	Student student2("Pam", "Art", 3.6);
+
+	cout << student1.hasHonors() << endl;
+	cout << student2.hasHonors() << endl;
+
+	return 0;
+}
